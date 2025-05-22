726b4f60e3a0bf00daf1a26ad675c1fc2610af45
Mary_All <mariiayatsenko7@gmail.com>
2025-05-22 16:14:38 +0300

    add index file

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+`use strict`;
+const categories = document.querySelectorAll("#categories .item");
+console.log("Number of categories:", categories.length);
+categories.forEach(item => {
+    console.log("Category:", item.querySelector("h2").textContent);
+    console.log("Elements:", item.querySelectorAll("ul li").length);
+});
